93ed809dd1047b336d6739bb6089398ab3144630
harioctav <aldiamahari16@gmail.com>
2024-10-28 20:25:56 +0700

    Install Filament & Setup Users Table

---
+++ app/Traits/EnumsToArray.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Traits;
+
+trait EnumsToArray
+{
+  public static function toArray(...$indexes)
+  {
+    $cases = self::cases();
+    $selectedCases = [];
+
+    foreach ($indexes as $index) {
+      if (array_key_exists($index, $cases)) {
+        $selectedCases[] = $cases[$index]->value;
+      }
+    }
+
+    if (empty($selectedCases)) {
+      return array_map(
+        fn(self $enum) => $enum->value,
+        $cases
+      );
+    }
+
+    return $selectedCases;
+  }
+
+  public static function toSelectArray(...$indexes)
+  {
+    $cases = self::cases();
+    $selectedCases = [];
+
+    foreach ($indexes as $index) {
+      if (array_key_exists($index, $cases)) {
+        $selectedCases[$cases[$index]->value] = $cases[$index]->value;
+      }
+    }
+
+    if (empty($selectedCases)) {
+      return array_reduce($cases, function ($carry, $item) {
+        $carry[$item->value] = $item->value;
+        return $carry;
+      }, []);
+    }
+
+    return $selectedCases;
+  }
+
+  public static function toValidation(...$indexes)
+  {
+    $cases = self::cases();
+    $selectedCases = [];
+
+    foreach ($indexes as $index) {
+      if (array_key_exists($index, $cases)) {
+        $selectedCases[] = $cases[$index]->value;
+      }
+    }
+
+    if (empty($selectedCases)) {
+      return 'in:' . implode(',', array_map(
+        fn(self $enum) => $enum->value,
+        $cases
+      ));
+    }
+
+    return 'in:' . implode(',', $selectedCases);
+  }
+}
